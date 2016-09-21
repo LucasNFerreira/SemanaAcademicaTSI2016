0d4639e3c4c0a47683594d6c42278c8058103343
Lucas N Ferreira <ljnferreira@gmail.com>
2016-09-20 23:44:08 -0300

    adicionando conteudo de hoje

--- AngularJS/EX2/js/exercicio2.js
+++ AngularJS/EX2/js/exercicio2.js
@@ -1,4 +1,4 @@
-let app = angular.module("meuPrimeiroApp", []);
+let app = angular.module('meuPrimeiroApp', ['ngRoute', 'ngMaterial']);
 app.factory('FacRepos', function($http){
     return function(url, scope){
         return{
@@ -17,16 +17,76 @@ app.factory('FacRepos', function($http){
     }
 });
 
-app.directive('listDirective', function(){
-    return{
-        restrict: "AE",
-        template: '<ul><li ng-repeat="repo in repos | filter:{name: termo }"><a href="{{repo.html_url}}"> {{ repo.name }}</a></li></ul>'
+app.factory('FacFollowers', function($http){
+    return function(url, scope){
+        return{
+            url: url,
+            scope: scope,
+            get: function(){
+            $http.get(url)
+                .then(function successCallback(response) {
+                    scope.followers = response.data;
+                    console.log(scope.followers);
+                }, function errorCallback(response) {
+                    console.log('Error: ' + response);
+                });
+          }
+        }
     }
 });
 
-app.controller("primeiroCtrl", function($scope, $http, FacRepos){
-    $scope.repos = [];
-    var reposGithub =
-    FacRepos("https://api.github.com/users/arthurassuncao/repos?client_id=8535bac83251149ac427&client_secret=0e7a3f067fb90c1a2f45c2e771dff96bed954989", $scope);
-    reposGithub.get();
+app.directive('listRepos', function(){
+    return{
+        restrict: "AEC",
+        templateUrl: 'templates/repo.tmpl.html'
+    }
 });
+
+app.directive('listSeguidores', function(){
+    return{
+        restrict: "AEC",
+        templateUrl: 'templates/follow.tmpl.html'
+    }
+});
+
+app.controller("primeiroCtrl", function($scope, $http, $location){
+    $scope.clickRepos = function() {
+        $location.url("/repos");
+    }
+
+    $scope.clickSeguidores = function() {
+        $location.url("/seguidores");
+    }
+});
+
+app.controller("reposCtrl", function($scope, $http, FacRepos){
+    $scope.repos = [];
+    var reposGithub = FacRepos(
+    "https://api.github.com/users/mateus/repos?client_id=8535bac83251149ac427&client_secret=0e7a3f067fb90c1a2f45c2e771dff96bed954989",
+    $scope);
+    reposGithub.get();
+    console.log("teste");
+});
+
+app.controller("seguidoresCtrl", function($scope, $http, FacFollowers){
+    $scope.followers = [];
+    var followersGithub = FacFollowers(
+    "https://api.github.com/users/mateus/followers?client_id=8535bac83251149ac427&client_secret=0e7a3f067fb90c1a2f45c2e771dff96bed954989",
+    $scope);
+    followersGithub.get();
+});
+
+app.config( [ '$routeProvider' , function($routerProvider){
+$routerProvider
+    .when('/repos', {
+        controller: "reposCtrl",
+        templateUrl: 'templates/repos.html'
+    })
+    .when('/seguidores', {
+        controller: "seguidoresCtrl",
+        templateUrl: 'templates/seguidores.html'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
